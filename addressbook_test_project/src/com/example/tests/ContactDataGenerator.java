3bd80f8441e2baee691baa10137751b5defc2354
olly-cholly <olga89able@gmail.com>
2015-08-09 19:37:34 +0300

    ContactGenerator and GroupGenerator was added

---
+++ addressbook_test_project/src/com/example/tests/ContactDataGenerator.java
@@ -0,0 +1,83 @@
+package com.example.tests;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ContactDataGenerator {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 3) {
+			System.out.println("Please specify parameters: <amount of tast data> <file> <format>");
+			return;
+		}
+		
+		int amount = Integer.parseInt(args[0]);
+		File file = new File(args[1]);
+		String format = args[2];
+		
+		if (file.exists()) {
+			System.out.println("File exists, please remote it manualy: " + file);
+			return;
+		}
+			
+		List<ContactData> contacts = generateRandomContact(amount);
+		if ("csv".equals(format)) {
+			saveContactToCsvFile(contacts, file);
+		} else if ("xml".equals(format)) {
+			saveContactToXmlFile(contacts, file);
+		} else {
+			System.out.println("Unknown format" + format);
+			return;
+		}
+	}
+
+	private static void saveContactToXmlFile(List<ContactData> contacts, File file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void saveContactToCsvFile(List<ContactData> contacts, File file) throws IOException {
+		FileWriter writer = new FileWriter(file);
+		for (ContactData contact : contacts) {
+			writer.write(contact.getLastname() + "," + contact.getFirstname() + "," + contact.getAddress1() + "," + contact.getHome() + "," + contact.getMobile() + "," + contact.getWork() + "," + contact.getEmail1() + "," + contact.getEmail2() + "," + contact.getBday() + "," + contact.getBmonth() + "," + contact.getByear() + "," + contact.getGroup() + "," 
+					+ contact.getAddress2() + "," + contact.getPhone() + "\n");			
+		}
+		writer.close();
+	}
+
+	public static List<ContactData> generateRandomContact(int amount) {
+		List<ContactData> list = new ArrayList<ContactData>();
+		for (int i = 0; i < amount; i++) {
+			ContactData contact = new ContactData()
+				.withLastname(generateRandomString())
+				.withFirstname(generateRandomString())
+				.withAddress1(generateRandomString())
+				.withHome(generateRandomString())
+				.withMobile(generateRandomString())
+				.withWork(generateRandomString())
+				.withEmail1(generateRandomString())
+				.withEmail2(generateRandomString())
+				.withBday("-")
+				.withBmonth("-")
+				.withByear(generateRandomString())
+				.withGroup("")
+				.withAddress2(generateRandomString())
+				.withPhone(generateRandomString());
+			list.add(contact);
+		}
+		return list;
+		}
+
+	public static String generateRandomString() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return "test" + rnd.nextInt();
+		}
+	}
+}
--- addressbook_test_project/src/com/example/tests/TestBase.java
+++ addressbook_test_project/src/com/example/tests/TestBase.java
@@ -3,13 +3,12 @@ package com.example.tests;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
-
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-
 import com.example.fw.ApplicationManager;
+import static com.example.tests.ContactDataGenerator.generateRandomContact;
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 
 public class TestBase {
 	
@@ -27,48 +26,28 @@ public class TestBase {
 	
 	@DataProvider
 	public Iterator<Object[]> randonValidContactGenerator() {
+		return wrapContactForDataProvidor(generateRandomContact(5)).iterator();
+	}
+	
+	private List<Object[]> wrapContactForDataProvidor(List<ContactData> contacts) {
 		List<Object[]> list = new ArrayList<Object[]>();
-		for (int i = 0; i < 5; i++) {
-			ContactData contact = new ContactData()
-				.withLastname(generateRandomString())
-				.withFirstname(generateRandomString())
-				.withAddress1(generateRandomString())
-				.withHome(generateRandomString())
-				.withMobile(generateRandomString())
-				.withWork(generateRandomString())
-				.withEmail1(generateRandomString())
-				.withEmail2(generateRandomString())
-				.withBday("-")
-				.withBmonth("-")
-				.withByear(generateRandomString())
-				.withGroup("")
-				.withAddress2(generateRandomString())
-				.withPhone(generateRandomString());
+		for (ContactData contact : contacts) {
 			list.add(new Object[]{contact});
 		}
-		return list.iterator();
+		return list;
 	}
-	
+
 	@DataProvider
 	public Iterator<Object[]> randonValidGroupGenerator() {
-		List<Object[]> list = new ArrayList<Object[]>();
-		for (int i = 0; i < 5; i++) {
-			GroupData group = new GroupData()
-				.withName(generateRandomString())
-				.withHeader(generateRandomString())
-				.withFooter(generateRandomString());
-				list.add(new Object[] {group});
-		}
-		return list.iterator();
+		return wrapGroupForDataProvidor(generateRandomGroups(5)).iterator();
 	}
 	
-	public String generateRandomString() {
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			return "";
-		} else {
-			return "test" + rnd.nextInt();
+	private List<Object[]> wrapGroupForDataProvidor(List<GroupData> groups) {
+		List<Object[]> list = new ArrayList<Object[]>();
+		for (GroupData group : groups) {
+			list.add(new Object[]{group});
 		}
+		return list;
 	}
-  
+	
 }
